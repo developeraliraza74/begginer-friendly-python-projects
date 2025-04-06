0948b3cbe05d6828137c61a0c519033bd04022df
Ali Raza <devaliraza74@gmail.com>
2025-04-06 05:07:44 +0500

    Update celsius to farenheight converter.py

--- celsius to farenheight converter.py
+++ celsius to farenheight converter.py
@@ -1,12 +1,12 @@
-def celsius_to_farenheight(celsius):
-    f = float(celsius)
-    c = (f-32) * 5/9
-    return c
-
-def farenheight_to_celsius(farenheight):
-    c = float(farenheight)
+def celsius_to_fahrenheit(celsius):
+    c = float(celsius)
     f = (c * 9/5) + 32
     return f
+
+def fahrenheit_to_celsius(fahrenheit):
+    f = float(fahrenheit)
+    c = (f - 32) * 5/9
+    return c
 
 
 
@@ -17,11 +17,11 @@ choice = int(input("Enter your choice: "))
 
 if choice == 1:
     celsius = float(input("Enter the temperature in Celsius: "))
-    farenheight = celsius_to_farenheight(celsius)
+    farenheight = celsius_to_fahrenheit(celsius)
     print(f"{celsius}C is {farenheight}F")
 elif choice == 2:
     farenheight = float(input("Enter the temperature in Farenheight: "))
-    celsius = farenheight_to_celsius(farenheight)
+    celsius = fahrenheit_to_celsius(farenheight)
     print(f"{farenheight}F is {celsius}C")
 else:
     print("Invalid choice")
